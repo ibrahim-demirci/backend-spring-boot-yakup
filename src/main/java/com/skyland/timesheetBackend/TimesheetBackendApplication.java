c877cadca52bd29f8680abd6ac06e4aec7d10042
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-25 13:57:36 +0300

    SPTSU-16 Create User, Roles and assign roles the users in code

--- src/main/java/com/skyland/timesheetBackend/TimesheetBackendApplication.java
+++ src/main/java/com/skyland/timesheetBackend/TimesheetBackendApplication.java
@@ -1,7 +1,15 @@
 package com.skyland.timesheetBackend;
 
+import com.skyland.timesheetBackend.domain.Role;
+import com.skyland.timesheetBackend.domain.User;
+import com.skyland.timesheetBackend.service.role.RoleService;
+import com.skyland.timesheetBackend.service.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class TimesheetBackendApplication {
@@ -10,4 +18,24 @@ public class TimesheetBackendApplication {
 		SpringApplication.run(TimesheetBackendApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner run(UserService userService, RoleService roleService) {
+		return args -> {
+			roleService.saveRole(new Role(null,"ROLE_USER"));
+			roleService.saveRole(new Role(null,"ROLE_ADMIN"));
+
+
+			userService.saveUser(new User(null,"John","Travolta", "555 555 55 55","description","john", "1234", new ArrayList<>(),false));
+			userService.saveUser(new User(null,"Will","Smith", "555 555 55 55","description","will", "1234", new ArrayList<>(),false));
+			userService.saveUser(new User(null,"Jim","Carry", "555 555 55 55","description","jim", "1234", new ArrayList<>(),false));
+			userService.saveUser(new User(null,"Arnold","Sch", "555 555 55 55","description","arnold", "1234", new ArrayList<>(),false));
+
+			roleService.addRoleToUser("john", "ROLE_USER");
+			roleService.addRoleToUser("will", "ROLE_USER");
+			roleService.addRoleToUser("jim", "ROLE_ADMIN");
+			roleService.addRoleToUser("arnold", "ROLE_ADMIN");
+			roleService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
 }
--- src/main/java/com/skyland/timesheetBackend/repo/RoleRepo.java
+++ src/main/java/com/skyland/timesheetBackend/repo/RoleRepo.java
@@ -5,5 +5,5 @@ import com.skyland.timesheetBackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String username);
+    Role findByName(String roleName);
 }
