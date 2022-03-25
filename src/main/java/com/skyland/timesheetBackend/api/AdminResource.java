7877d05e3b854a3b74b3c2228199d5343f3bae9d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-25 13:18:16 +0300

    SPTSU-16 Create AdminResource for endpoints and add getUsers method.

---
+++ src/main/java/com/skyland/timesheetBackend/api/AdminResource.java
@@ -0,0 +1,24 @@
+package com.skyland.timesheetBackend.api;
+
+
+import com.skyland.timesheetBackend.domain.User;
+import com.skyland.timesheetBackend.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api") @RequiredArgsConstructor
+public class AdminResource {
+
+    private final AdminService adminService;
+
+    @GetMapping("/admin/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(adminService.getUsers());
+    }
+}
