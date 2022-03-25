a454f996d87b356126db39a0d54ea04cf6754c52
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-25 14:54:58 +0300

    SPTSU-16 Encode user password for security credential

--- src/main/java/com/skyland/timesheetBackend/service/user/UserService.java
+++ src/main/java/com/skyland/timesheetBackend/service/user/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,7 +17,9 @@ import java.util.Collection;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
 public class UserService implements BaseUserService, UserDetailsService {
+
     private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -37,6 +40,7 @@ public class UserService implements BaseUserService, UserDetailsService {
 
     @Override
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
