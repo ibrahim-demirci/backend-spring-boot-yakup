ef3883d48edc75232a421e71101bdb7f8678fa1b
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-25 12:12:45 +0300

    Initial commit

---
+++ src/main/java/com/skyland/timesheetBackend/TimesheetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skyland.timesheetBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetBackendApplication.class, args);
+	}
+
+}
