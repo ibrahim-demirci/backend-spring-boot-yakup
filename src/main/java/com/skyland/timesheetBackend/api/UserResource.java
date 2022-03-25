99b21c268f02c4535c1aaa6f71744984c0d6d847
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-25 13:33:20 +0300

    SPTSU-16 Create RoleResource and UserResource

---
+++ src/main/java/com/skyland/timesheetBackend/api/UserResource.java
@@ -0,0 +1,26 @@
+package com.skyland.timesheetBackend.api;
+
+
+import com.skyland.timesheetBackend.domain.User;
+import com.skyland.timesheetBackend.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController @RequestMapping("/api") @RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+    @GetMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+
+
+}
