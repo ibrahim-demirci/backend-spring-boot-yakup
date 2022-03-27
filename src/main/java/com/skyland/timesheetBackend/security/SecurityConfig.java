65fc63c9affc8e721b7d32df9f0e7450f585dd28
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-27 17:07:09 +0300

    SPTSU-26 Change login path

--- src/main/java/com/skyland/timesheetBackend/security/SecurityConfig.java
+++ src/main/java/com/skyland/timesheetBackend/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/api/authenticate/login");
+        customAuthenticationFilter.setFilterProcessesUrl("/api/authenticate/signin");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().antMatchers("/api/login/**", "api/token/refresh/**").permitAll();
