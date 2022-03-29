edfcaf6146cadae51607e5ad8b2f7c5d4e910eb3
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-29 11:02:49 +0300

    SPTSU-26 Add unknown error type and info

--- src/main/java/com/skyland/timesheetBackend/utilities/ErrorMessageUtilities.java
+++ src/main/java/com/skyland/timesheetBackend/utilities/ErrorMessageUtilities.java
@@ -1,17 +1,17 @@
-package com.skyland.timesheetBackend.api.utilities;
+package com.skyland.timesheetBackend.utilities;
 
-public final class ErrorMessage {
+public final class ErrorMessageUtilities {
 
     public static class ErrorMessageType {
         public static final String USERNAME_ALREADY_TAKEN = "username-already-taken";
         public static final String USER_NOT_FOUND = "user-not-found";
+        public static final String UNKNOWN_ERROR = "unknown-error";
     }
 
     public static class ErrorMessageInfo {
         public static final String USERNAME_ALREADY_TAKEN_INFO = "This username is already taken.";
-        public static final String USER_NOT_FOUND_INFO = "User not found with username. Please sign up before.";
+        public static final String USER_NOT_FOUND_INFO = "User not found with username.";
+        public static final String UNKNOWN_ERROR_INFO = "An unknown error.";
     }
-
-
 
 }
